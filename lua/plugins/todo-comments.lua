2299ddcda1280397c71a18f211c1363967f7cca9
Iustin <iustin.esanu7@gmail.com>
2024-01-11 18:04:42 +0200

    fixed toggling

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,22 +1,7 @@
 return {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-    opts = {},
-    config = function()
-        local todo = require("todo-comments")
-        vim.keymap.set("n", "]t", function()
-            todo.jump_next()
-        end, { desc = "Next [T]odo comment" })
-
-        vim.keymap.set("n", "[t", function()
-            todo.jump_prev()
-        end, { desc = "Previous [T]odo comment" })
-
-        -- You can also specify a list of valid jump keywords
-
-        vim.keymap.set("n", "]te", function()
-            todo.jump_next({ keywords = { "ERROR" } })
-        end, { desc = "[N]ext [E]rror" })
-    end
-
+    opts = {
+        vim.keymap.set("n", "<leader>td", ":TodoTrouble<CR>", {desc = "[T]oggle To[D]o"})
+    },
 }
